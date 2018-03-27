5dd571a51176933b44ab0c912beeff62c85af6e5
Noemi Romero <nlll02@yahoo.com>
2018-03-27 14:53:59 -0500

    created my tomagachi

--- js/app.js
+++ js/app.js
@@ -2,5 +2,25 @@ console.log('creation')
 
 
 class Tomagachi {
-	constructor(size, name, age, food)
+	constructor(hunger, sleepiness, boredom, age){
+	this.hunger = hunger;	
+	this.sleepiness = sleepiness;
+	this.boredom = boredom;
+	this.age = age;
+		
+	}
+	jump(){
+		console.log('I can jump')
+	}
+	walk(){
+
+	}
+	
+	
+
 }
+console.log(Tomagachi)
+const shadow = new Tomagachi(5, 3, 1, 7)
+
+console.log(shadow)
+	

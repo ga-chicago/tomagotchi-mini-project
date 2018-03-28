469d2af70bdb369213a969561adeb07df3df934e
Noemi Romero <nlll02@yahoo.com>
2018-03-28 18:13:10 -0500

    time is running

--- js/app.js
+++ js/app.js
@@ -1,26 +1,35 @@
 console.log('creation')
+let timer;
+let time = 0;
+// let newAge = 3;
 
-$('button').on('click', (event) => {
+$('#food').on('click', (event) => {
 	console.log('hey')
 	const feed = $('input').val();
 	shadow.feedMe(feed);
-})
-
-
-$('button').on('click', (event) => {
-	console.log('hey')
-	const feed = $('input').val();
 
 })
 
-$('button').on('click', (event) => {
-	console.log('hey')
+
+$('#light').on('click', (event) => {
+	console.log('boo')
+	const lights = $('input').val();
+
+})
+
+$('#play').on('click', (event) => {
+	console.log('ball')
 	const playWithMe = $('input').val();
 	shadow.play(playWithMe);
 
 })
 
+$('#name').on('click', (event) => {
+	console.log('hola')
+	const changeName = $('input').val();
+	shadow.name = changeName;
 
+})
 
 class Tomagachi {
 	constructor(hunger, sleepiness, boredom, age){
@@ -28,39 +37,116 @@ class Tomagachi {
 		this.sleepiness = sleepiness;
 		this.boredom = boredom;
 		this.age = age;
+		
 			
 	}
-	jump(){
-		console.log('I can jump')
+	sleep(){
+		// if(this)
 	}
 	feedMe(){
-		console.log('I am hungry')
+		// reduce hunger
+		if(this.hunger > 0) {
+			this.hunger--
+		}
+		// increase sleepiness
+		if(this.sleepiness < 10){
+			this.sleepiness++	
+		}
+		
+		printInfo();
 
 	}
 	play(action){
 		console.log('play with me')
 	}
 	
+	
 
 }
 //console.log(Tomagachi)
 const shadow = new Tomagachi(5, 3, 1, 7)
-// add the value for hunger to the Hunger h1 in the html using jQuery
+console.log(shadow);
+
+
+const printInfo = () => {
+	// add the value for hunger to the Hunger h1 in the html using jQuery
 	// grab the correct html element w/ jq
-const hunger = $('#hunger')
-hunger.text(shadow.hunger)
-// if(hunger = 5)
-// add the value for sleepiness to the sleepiness h1 in the html using jQuery
-const sleepiness = $('#sleepiness')
-console.log(shadow.sleepiness, ' this is sleepy')
-sleepiness.text(shadow.sleepiness)
-// add the value for age to the age h1 in the html using jQuery
-const age = $('#age')
-age.text(shadow.age)
+	const hunger = $('#hunger')
+	hunger.text('hunger: ' + shadow.hunger)
+	// if(hunger = 5)
+	// add the value for sleepiness to the sleepiness h1 in the html using jQuery
+	const sleepiness = $('#sleepiness')
+	console.log(shadow.sleepiness, ' this is sleepy ')
+	sleepiness.text('sleepiness: ' + shadow.sleepiness)
+	// add the value for age to the age h1 in the html using jQuery
+	const age = $('#age')
+	age.text('age: ' + shadow.age)
 
 
-// add the value for boredom to the boredom h1 in the html using jQuery
-const boredom = $('#boredom')
-boredom.text(shadow.boredom)
-console.log(shadow)
+	// add the value for boredom to the boredom h1 in the html using jQuery
+	const boredom = $('#boredom')
+	boredom.text('boredom: ' + shadow.boredom)
+
+	console.log(shadow)
+}
+
+
+
+
+    //   clearInterval(timer)
+
+const setTimer = () => {
 	
+  timer = setInterval(() => {
+    time++
+    console.log( time + ' timer is running')
+    
+    //   shadow.age++;
+    
+    if(time === 60){
+    	shadow.age++
+    	printInfo();
+    }
+
+    // $('#timer').text('timer: ' + time + 's');
+
+  }, 1000);
+
+};
+
+
+printInfo();
+setTimer();
+
+// for(let i = 0; i < 100; i++) {
+// 	// if the remainder when you divide i by 2 is 0
+// 	if(i % 2 == 0) { // (even)
+// 		console.log(i)
+// 	}
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

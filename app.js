8e8f779d08404fe3bdbf01af860633f7c93f4c2e
Irwin <irgranados16@gmail.com>
2018-03-27 21:39:04 -0500

    finished mvp

--- app.js
+++ app.js
@@ -1,11 +1,88 @@
 console.log("its working");
+// const button = $('button')
 
 
 class Tamagotchi {
-	constructor(hunger, sleepiness, boredom, age){
-		this.hunger = hunger;
-		this.sleepiness = sleepiness;
-		this.boredom = boredom;
-		this.age = age;
+	constructor(petName){
+		this.hunger = 1;
+		this.sleepiness = 1;
+		this.boredom = 1;
+		this.age = 1;
+		this.petName = petName;
+		this.isAlive = true;
+	}
+	feed() {
+		return this.hunger--
+	}
+	sleep() {
+		return this.sleepiness--
+	}
+	play() {
+		return this.boredom--
+	}
+	life() {
+		return this.age++
+	}
+	setName(newName) {
+		return this.petName = newName
+	}
+	killPet() {
+		return this.isAlive = false
 	}
 }
+
+const pet = new Tamagotchi('irwin');
+
+// console.log(irwin);
+
+// $('button').on('click', (e) => {
+// 	const action = $(e.currentTarget).text();
+// 	console.log(action);
+// })
+
+$('#feed').on('click', () => {
+	pet.feed()
+	console.log(pet.hunger);
+})
+
+$('#light').on('click', () => {
+	pet.sleep()
+	console.log(pet.sleepiness);
+})
+
+$('#play').on('click', () => {
+	pet.play()
+	console.log(pet.boredom);
+})
+
+$('#setName').on('click', (event) => {
+	pet.setName($('input').val());
+	window.alert(pet.petName);
+})
+
+
+function agePet() {
+	setInterval(function(){ 
+		if (pet.isAlive) {
+			pet.age++
+		}
+	}, 60000);
+}
+agePet()
+
+setInterval(function(){ 
+	if (pet.isAlive) {
+		pet.hunger++
+		pet.sleepiness++
+		pet.boredom++
+
+		if (pet.hunger >= 10 || pet.boredom >= 10 || pet.sleepiness >= 10) {
+			pet.killPet()	
+		}	
+	}
+}, 2000);
+
+
+
+
+
